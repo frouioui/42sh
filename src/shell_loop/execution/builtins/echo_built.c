bc2a630318c56cd921f35ae5a1d463831ad17231
Cécile Cadoul <cecile.cadoul@epitech.eu>
2018-04-25 11:47:36 +0200

    MOD [echo] :fix segfault "echo -n"

--- src/shell_loop/execution/builtins/echo_built.c
+++ src/shell_loop/execution/builtins/echo_built.c
@@ -14,7 +14,7 @@
 static bool search_option(pipe_t *pipe, int *index)
 {
 	if (pipe->args[1] != NULL) {
-		if (strcmp(pipe->args[1], "-n") == 0) {
+	 	if (strcmp(pipe->args[1], "-n") == 0) {
 			*index += 1;
 			return (true);
 		}
@@ -22,25 +22,32 @@ static bool search_option(pipe_t *pipe, int *index)
 	return (false);
 }
 
-int echo_built(shell_t *shell, pipe_t *pipe)
+static void write_args(pipe_t *pipe)
 {
 	int index = 1;
 	bool n_option = false;
 
+	check_quote(pipe->args);
+	n_option = search_option(pipe, &index);
+	while (pipe->args[index] != NULL &&
+	pipe->args[index + 1] != NULL) {
+		write(pipe->fd, pipe->args[index],
+			strlen(pipe->args[index]));
+		write(pipe->fd, " ", 1);
+		index += 1;
+	}
+	if (pipe->args[index] != NULL)
+		write(pipe->fd, pipe->args[index], strlen(pipe->args[index]));
+	if (n_option != true)
+		write(pipe->fd, "\n", 1);
+}
+
+int echo_built(shell_t *shell, pipe_t *pipe)
+{
 	if (pipe->args[1] == NULL)
 		write(pipe->fd, "\n", 1);
 	else {
-		check_quote(pipe->args);
-		n_option = search_option(pipe, &index);
-		while (pipe->args[index + 1] != NULL) {
-			write(pipe->fd, pipe->args[index],
-				strlen(pipe->args[index]));
-			write(pipe->fd, " ", 1);
-			index += 1;
-		}
-		write(pipe->fd, pipe->args[index], strlen(pipe->args[index]));
-		if (n_option != true)
-			write(pipe->fd, "\n", 1);
+		write_args(pipe);
 	}
 	return (0);
 }
