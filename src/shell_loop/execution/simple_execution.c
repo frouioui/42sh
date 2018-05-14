f8bccbe3fefde342da5c47c417e1951965b713a7
Florent Poinsard <florent.poinsard@outlook.fr>
2018-05-14 08:25:43 +0200

    PATCH [conditions] : better conditional system during the execution.

--- src/shell_loop/execution/simple_execution.c
+++ src/shell_loop/execution/simple_execution.c
@@ -40,6 +40,8 @@ void simple_execution(shell_t *shell, instruction_t *instruction)
 	if (is_builtins(instruction->pipe[0]->args[0]) == true) {
 		if (exec_builtins(shell, instruction->pipe[0]) == -1)
 			shell->code = 1;
+		else
+			shell->code = 0;
 	} else if ((instruction->pipe[0]->path_exec =
 		get_path_exec(instruction->pipe[0], shell)) != NULL) {
 		execute_child(shell, instruction->pipe[0]);
